cc37f865ee62d042966ec82b9e5d2d3c04bef069
SundharAK <sundhar.aravazhi@gmail.com>
2026-03-18 07:33:54 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="card">
+        <h1>AWS CI/CD Demo</h1>
+        <h2>Hi, I'm Sundhar</h2>
+        <p>This React app is deployed using CodePipeline</p>
+
+        <button>Deployment Successful ✅</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
